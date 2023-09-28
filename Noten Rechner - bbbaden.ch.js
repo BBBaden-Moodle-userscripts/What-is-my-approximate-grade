5f3e4314b1c6a743d1f37430d0a87e6f533f492d
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2023-09-28 20:36:11 +0200

    Update Noten Rechner - bbbaden.ch.js
    Version: 1.0
    
    fixed updates

--- Noten Rechner - bbbaden.ch.js
+++ Noten Rechner - bbbaden.ch.js
@@ -2,16 +2,16 @@
 // @name        Noten Rechner - bbbaden.ch
 // @namespace   Violentmonkey Scripts
 // @match       https://moodle.bbbaden.ch/mod/quiz/review.php*
-// @version     1.1
-// @description 18.9.2023, 08:51:00
+// @version     1.0
+// @description Noten Rechner für Moodle
 // @icon        https://uploads-ssl.webflow.com/61add382915b0a19b218de1e/64726a127f34ac9eb1149e84_test-score-sheet-with-answers-grade-a-and-pencil-2021-09-02-21-27-37-utc%20(1).jpg
 //
 // @author          Pianonic (https://github.com/Pianonic)
-// @namespace       https://github.com/black-backdoor/BBBaden-Moodle-userscripts/
-// @supportURL      https://github.com/black-backdoor/BBBaden-Moodle-userscripts/issues
-// @homepageURL     https://github.com/black-backdoor/BBBaden-Moodle-userscripts/
-// @downloadURL     https://raw.githubusercontent.com/black-backdoor/BBBaden-Moodle-userscripts/main/scripts/Noten%20Rechner%20-%20bbbaden.ch.js
-// @updateURL       https://raw.githubusercontent.com/black-backdoor/BBBaden-Moodle-userscripts/main/scripts/Noten%20Rechner%20-%20bbbaden.ch.js
+// @namespace       https://github.com/BBBaden-Moodle-userscripts/Noten-Rechner/
+// @supportURL      https://github.com/BBBaden-Moodle-userscripts/Noten-Rechner/issues
+// @homepageURL     https://github.com/BBBaden-Moodle-userscripts/Noten-Rechner/
+// @downloadURL     https://github.com/BBBaden-Moodle-userscripts/Noten-Rechner/raw/main/Noten%20Rechner%20-%20bbbaden.ch.js
+// @updateURL       https://github.com/BBBaden-Moodle-userscripts/Noten-Rechner/raw/main/Noten%20Rechner%20-%20bbbaden.ch.js
 //
 // @compatible      firefox
 // @grant           GM_getValue
